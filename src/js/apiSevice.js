dbb81c61bd4f0bb5569f84537b8bf4ac7d0a59d7
Aleksandr Kadun <aleksandrkadun@gmail.com>
2021-01-25 02:44:48 +0200

    change resultLength

--- src/js/apiSevice.js
+++ src/js/apiSevice.js
@@ -2,7 +2,7 @@ const authKey = '20005149-bc71a0b9546335ae16bac5ccc'
 export default {
     searchReq: "",
     page: 1,
-    resultLength:'15',
+    resultLength:'12',
     imgSearching() {    
         const requestUrl = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchReq}&page=${this.page}&per_page=${this.resultLength}&key=${authKey}`
         return fetch(requestUrl).then(resp => resp.json()).then(data => { 
